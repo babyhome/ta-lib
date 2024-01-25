6a2b22583e7c9cf62e752fe42e8cfad570331ea7
sicr.beerz <dek.baan.baan@gmail.com>
2024-01-25 13:18:50 +0700

    Init

---
+++ src/main/java/com/brz/talib/TaLibApplication.java
@@ -0,0 +1,13 @@
+package com.brz.talib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brz/talib/TaLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brz.talib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
